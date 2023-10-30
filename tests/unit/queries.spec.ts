37486b91f0f119618ba1f1b1c6c790c5dc8268c6
Nicola Marcacci Rossi <nicola@smartive.ch>
2023-10-30 13:07:21 +0100

    fix

--- tests/unit/queries.spec.ts
+++ tests/unit/queries.spec.ts
@@ -4,7 +4,7 @@ import { models } from '../utils/models';
 describe('queries', () => {
   describe('getEntityRelationsQuery', () => {
     it('applies filters', () => {
-      expect(getEditEntityRelationsQuery(models, models.getModel('SomeObject', 'entity'), 'update')).toMatchSnapshot();
+      expect(getEditEntityRelationsQuery(models.getModel('SomeObject', 'entity'), 'update')).toMatchSnapshot();
     });
   });
 });
