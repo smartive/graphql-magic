ffabfff37e64128616353cdeb3a9ddf4c58d96ba
Nicola Marcacci Rossi <nicola@smartive.ch>
2024-04-03 13:20:34 +0200

    fix

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
           items: [
             {
               label: 'Docs',
-              to: '/docs/intro',
+              to: '/docs/tutorial',
             },
           ],
         },
