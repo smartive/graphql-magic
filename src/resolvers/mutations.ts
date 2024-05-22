a59289fae7a1e3bafc35c9bdf2fb78af9e82170c
Dominique Wirz <dominique@smartive.ch>
2024-05-22 10:12:14 +0200

    fix: pick required context properties for createRevision

--- src/resolvers/mutations.ts
+++ src/resolvers/mutations.ts
@@ -316,7 +316,7 @@ const restore = async (model: EntityModel, { where }: { where: any }, ctx: FullC
   return entity.id;
 };
 
-export const createRevision = async (model: EntityModel, data: Entity, ctx: Context) => {
+export const createRevision = async (model: EntityModel, data: Entity, ctx: Pick<Context, 'knex' | 'now' | 'user'>) => {
   if (model.updatable) {
     const revisionId = uuid();
     const rootRevisionData: Entity = {
